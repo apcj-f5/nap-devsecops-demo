c62f0bcf268f091b7c218d8a86ede2c034407411
Leon Seng <w.leon.seng@gmail.com>
2023-06-01 22:34:14 +1000

    Updated tag-for-deployment gh action to check for pre-commit scan result

--- .github/scripts/check-workflows.sh
+++ .github/scripts/check-workflows.sh
@@ -15,6 +15,8 @@ start_time=$(date +%s)
 check_workflow_completion() {
   local workflow_name=$1
 
+  echo "Checking workflow: $workflow_name"
+
   while true; do
     current_time=$(date +%s)
     elapsed_time=$((current_time - start_time))
@@ -53,12 +55,32 @@ check_workflow_completion() {
 
 }
 
-echo "Checking Semgrep"
 check_workflow_completion "Semgrep"
-
-echo "OpenSSF Scorecard"
 check_workflow_completion "OpenSSF Scorecard"
 
+echo "Checking pre-commit result"
+while true; do
+  current_time=$(date +%s)
+  elapsed_time=$((current_time - start_time))
 
+  if [[ $elapsed_time -gt $max_wait_time ]]; then
+    echo "Timeout reached. Expected string 'success' not returned within $max_wait_time seconds."
+    exit 1
+  fi
 
-# check commit status set by pre-commit
+  echo "$(date): Checking status..."
+  response=$(curl --fail -s -L \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $GITHUB_PAT" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "https://api.github.com/repos/apcj-f5/hapi.f5labs.dev/commits/$GITHUB_SHA/status")
+
+  state=$(echo $response | jq -r '.statuses[] | select(.context == "pre-commit.ci - push") | .state'
+
+  if [[ $state == "success" ]]; then
+    echo "Expected state 'success' returned."
+    break
+  fi
+
+  sleep $check_interval
+done
